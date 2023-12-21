774c41100bd17dfd4e515275b9e95c3720df6aa6
Richard Walsh <richwalsh2@googlemail.com>
2023-12-21 21:41:48 +0000

    add back of card

--- src/components/RecipeCardFront/RecipeCardFront.tsx
+++ src/components/RecipeCardFront/RecipeCardFront.tsx
@@ -4,65 +4,132 @@ import recipeImg from '../../assets/img/white-chocolate-custard-tart.jpeg';
 
 const RecipeCardFront = () => {
   return (
-    <section className={classes.rc_front}>
-      <h2 className={classes.rc_front__heading}>
-        White chocolate custard tart
-      </h2>
-      <div className={classes.rc_front__img_container}>
-        <img src={recipeImg} />
-      </div>
+    <>
+      <section className={classes.rc_front}>
+        <div className={classes.rc_front__heading}>
+          <h2> White chocolate custard tart</h2>
+        </div>
+        <div className={classes.rc_front__img_container}>
+          <img src={recipeImg} />
+        </div>
 
-      <div className={classes.rc_front__information}>
-        <div className={classes.rc_front__description}>
-          A glorious custard tart made with white chocolate.
-        </div>
-        <ul>
-          <li>
-            <p>
-              <span>Serves:</span>&nbsp;8 Slices
-            </p>
-          </li>
-          <li>
-            <p>
-              <span>Preparation Time:</span>&nbsp;14h 10m
-            </p>
-          </li>
-          <li>
-            <p>
-              <span>Baking Time:</span>&nbsp;1h
-            </p>
-          </li>
-          <li>
-            <p>
-              <span>Oven Temperature</span>&nbsp;Preheat to 200c / 180c Fan
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div className={classes.rc_front__ingredients}>
-        <div>
-          <ul>
-            <li>1 1/2 cups plain flour, plus extra for dusting</li>
-            <li>2 tbsp caster sugar</li>
-            <li>125g unsalted butter, chilled, chopped</li>
-            <li>1 egg yolk</li>
-            <li>1 tbsp water, chilled</li>
-            <li>125g raspberries</li>
-            <li>40g white chocolate, curls</li>
-            <li>2/3 cup thickened cream</li>
+        <div className={classes.rc_front__information}>
+          <div className={classes.rc_front__description}>
+            A glorious custard tart made with white chocolate.
+          </div>
+          <ul role="list">
+            <li>
+              <p>
+                <span>Serves:</span>&nbsp;8 Slices
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>Preparation Time:</span>&nbsp;14h 10m
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>Baking Time:</span>&nbsp;1h
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>Oven Temp:</span>&nbsp;Preheat to 200c / 180c Fan
+              </p>
+            </li>
           </ul>
         </div>
-        <div>
-          <h3>White choclate custard</h3>
-          <ul>
-            <li>300ml pure cream</li>
-            <li>1/2 cup milk</li>
-            <li>2 x 180g blocks white chocolate, chopped</li>
-            <li>2 eggs, lightly beaten</li>
-          </ul>
+        <div className={classes.rc_front__ingredients}>
+          <div>
+            <ul className={classes.ingredients__main}>
+              <li>1 1/2 cups plain flour, plus extra for dusting</li>
+              <li>2 tbsp caster sugar</li>
+              <li>125g unsalted butter, chilled, chopped</li>
+              <li>1 egg yolk</li>
+              <li>1 tbsp water, chilled</li>
+              <li>125g raspberries</li>
+              <li>40g white chocolate, curls</li>
+              <li>2/3 cup thickened cream</li>
+            </ul>
+            <h3>White chocolate custard</h3>
+            <ul className={classes.ingredients__sub}>
+              <li>300ml pure cream</li>
+              <li>1/2 cup milk</li>
+              <li>2 x 180g blocks white chocolate, chopped</li>
+              <li>2 eggs, lightly beaten</li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {/* BACK OF CARD */}
+      <section>
+        <div className={classes.rc_back}>
+          <div className={classes.rc_front__heading}>
+            <h2> White chocolate custard tart</h2>
+          </div>
+          <div className={classes.rc_back__method}>
+            <ul>
+              <li>
+                <span>step 1</span>
+                <br />
+                Place flour, sugar and butter in a food processor. Process until
+                mixture resembles fine breadcrumbs. Add egg yolk and water,
+                adding an extra teaspoon of water if needed. Process until dough
+                just comes together. Turn out dough onto a lightly floured
+                surface. Knead until just smooth. Shape into a disc. Wrap in
+                baking paper. Refrigerate for 30 minutes.
+              </li>
+              <li>
+                <span>step 2</span>
+                <br />
+                Preheat oven to 200C/180C fan-forced. Grease a 3cm-deep, 23cm
+                round (base) loose-based fluted tart pan. Roll out pastry
+                between 2 sheets of baking paper until 4mm thick. Line base and
+                side of pan with pastry. Refrigerate for 10 minutes.
+              </li>
+              <li>
+                <span>step 3</span>
+                <br />
+                Meanwhile, make White chocolate custard; Place cream and milk in
+                a medium saucepan. Heat over medium-high heat until just
+                simmering (do not boil). Remove from heat. Add chocolate. Stir
+                until smooth and combined. Set aside for 15 minutes or until
+                cool. Whisk in egg.
+              </li>
+              <li>
+                <span>step 4</span>
+                <br />
+                Place tart pan on a baking tray. Line pastry case with baking
+                paper. Fill with pie weights or uncooked rice. Bake for 10 to 12
+                minutes or until edges are light golden. Remove weights and
+                paper. Bake for a further 5 to 7 minutes or until base is light
+                golden. Set aside to cool. Reduce oven to 170C/150C fan-forced.
+              </li>
+              <li>
+                <span>step 5</span>
+                <br />
+                Pour custard into pastry case. Bake for 40 to 45 minutes or
+                until just set. Cool to room temperature. Refrigerate overnight.
+              </li>
+              {/* <li>
+                <span>step 6</span>
+                <br />
+                Stand tart at room temperature for 10 minutes before serving.
+                Top with raspberries and grated white chocolate curls. Serve
+                with cream.
+              </li> */}
+            </ul>
+          </div>
+          <div className={classes.rc_back__footer}>
+            <p>Recipe from:</p>
+            <p>
+              https://www.taste.com.au/recipes/white-chocolate-custard-tart/d6b10593-790f-4ba0-a3bf-30e1eff28a12
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
